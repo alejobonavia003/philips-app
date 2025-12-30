1c4b1962936161a4e88c2672d8088189b7bfd533
alejobonavia003 <bonaviaalejo@gmail.com>
2025-12-30 12:47:28 -0300

    le puse el patch cada vez que el pool abre una coneccion

--- src/config/db.js
+++ src/config/db.js
@@ -11,22 +11,10 @@ const pool = new Pool({
   idleTimeoutMillis: 30000,
 });
 
-// ESTO ES LO QUE FALTA:
-const initDb = async () => {
-  try {
-    // Forzamos a que busque en tu esquema y luego en public
-    await pool.query("SET search_path TO philips_db, public;");
-    console.log("✅ Schema search_path configurado");
-  } catch (err) {
-    console.error("❌ Error configurando el search_path:", err);
-  }
-};
-
-initDb();
-
-// Verificación de conexión
-pool.on("connect", () => {
-  console.log("Conexion exitosa con Neon");
+// SOLUCIÓN: Usar el evento 'connect' para configurar la sesión
+// Cada vez que el pool entrega un cliente, ejecutamos esto:
+pool.on("connect", (client) => {
+  client.query("SET search_path TO philips_db, public;");
 });
 
 export const query = (text, params) => pool.query(text, params);
